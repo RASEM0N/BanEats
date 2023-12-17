bb881b316f99076ed66a712f0b760038a152d8e9
Роман <negativ12fly@gmail.com>
2023-12-17 15:53:37 +0500

    feat: добавил logger morgan

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as morgan from 'morgan';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+	app.use(morgan('dev'));
 	await app.listen(3000);
 }
 
-bootstrap().catch((e) => {
-	console.error('Ошибка старта приложения', e);
+bootstrap().then(() => {
+	const messages = [
+		'--------------------------------------------',
+		`Main server: http://localhost:${3000}`,
+		`Apollo server: http://localhost:${3000}/graphql`,
+		'--------------------------------------------',
+	];
+	messages.forEach((msg) => console.log(`${msg}`));
 });
