cfcefc966ab623bddee479d6c81f8114a05d5136
Роман <negativ12fly@gmail.com>
2023-12-17 16:55:22 +0500

    feat: настройка typeorm и config

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,17 +1,41 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver } from '@nestjs/apollo';
-import type { ApolloDriverConfig } from '@nestjs/apollo';
 import { RestaurantModule } from './restaurant/restaurant.module';
-
-// import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import type { ApolloDriverConfig } from '@nestjs/apollo';
+import * as joi from 'joi';
 
 @Module({
 	imports: [
+		ConfigModule.forRoot({
+			isGlobal: true,
+			envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.prod',
+			ignoreEnvFile: process.env.NODE_ENV === 'prod',
+			validationSchema: joi.object({
+				NODE_ENV: joi.string().valid('dev', 'prod', 'test').required(),
+				DB_HOST: joi.string().required(),
+				DB_NAME: joi.string().required(),
+				DB_PORT: joi.number().required(),
+				DB_USERNAME: joi.string().required(),
+				DB_PASSWORD: joi.string().required(),
+			}),
+		}),
 		GraphQLModule.forRoot<ApolloDriverConfig>({
 			driver: ApolloDriver,
 			autoSchemaFile: true,
-			// playground: true,
+			playground: true,
+		}),
+		TypeOrmModule.forRoot({
+			type: 'postgres',
+			database: process.env.DB_NAME,
+			host: process.env.DB_HOST,
+			port: +process.env.DB_PORT,
+			username: process.env.DB_USERNAME,
+			password: process.env.DB_PASSWORD,
+			synchronize: true,
+			logging: true,
 		}),
 		RestaurantModule,
 	],
