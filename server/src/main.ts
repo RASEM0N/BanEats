ecaccd28255df2cc2c00bacbae2daf861019f33e
Роман <negativ12fly@gmail.com>
2023-12-17 16:23:59 +0500

    feat: добавил валидацию

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,17 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as morgan from 'morgan';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.use(morgan('dev'));
+	app.useGlobalPipes(new ValidationPipe());
 	await app.listen(3000);
 }
 
 bootstrap().then(() => {
 	const messages = [
 		'--------------------------------------------',
-		`Main server: http://localhost:${3000}`,
+		`Main server:   http://localhost:${3000}`,
 		`Apollo server: http://localhost:${3000}/graphql`,
 		'--------------------------------------------',
 	];
--- server/src/restaurant/dtos/create.dto.ts
+++ server/src/restaurant/dtos/create.dto.ts
@@ -1,16 +1,24 @@
 import { ArgsType, Field } from '@nestjs/graphql';
+import { IsBoolean, IsString, Length } from 'class-validator';
 
 @ArgsType()
 export class CreateRestaurantDto {
 	@Field(() => String)
+	@IsString()
+	@Length(5, 100)
 	name: string;
 
 	@Field(() => Boolean)
+	@IsBoolean()
 	isVegan: boolean;
 
 	@Field(() => String)
+	@IsString()
+	@Length(5, 255)
 	address: string;
 
 	@Field(() => String)
+	@IsString()
+	@Length(5, 255)
 	ownersName: string;
 }
