de910d7b46aad483d00fd0d6af082ba4c9b607ed
Роман <negativ12fly@gmail.com>
2023-12-24 23:37:24 +0500

    feat: добавил verification entity...

---
+++ server/src/modules/users/entities/verification.entity.ts
@@ -0,0 +1,27 @@
+import { BeforeInsert, Column, Entity, JoinColumn, OneToOne } from 'typeorm';
+import { Field, ObjectType } from '@nestjs/graphql';
+import { CoreEntity } from '@/shared/entities/core.entity';
+import { User } from '@/modules/users/entities/user.entity';
+import { v4 as uuidv4 } from 'uuid';
+
+@ObjectType()
+@Entity({
+	name: 'verification',
+})
+export class Verification extends CoreEntity {
+	@Field(() => String)
+	@Column()
+	code: string;
+
+	@OneToOne(() => User, {
+		// если Подтверждения удалится то и пользователь тоже
+		onDelete: 'CASCADE',
+	})
+	@JoinColumn()
+	user: User;
+
+	@BeforeInsert()
+	private createCore(): void {
+		this.code = uuidv4();
+	}
+}
--- server/src/modules/users/users.service.ts
+++ server/src/modules/users/users.service.ts
@@ -1,19 +1,32 @@
-import { Injectable } from '@nestjs/common';
-import { Repository } from 'typeorm';
+import { Inject, Injectable } from '@nestjs/common';
+import { DataSource, Repository } from 'typeorm';
 import { User } from './entities/user.entity';
 import { CreateUserArgs } from './dtos/create.dto';
 import { DefaultCRUD } from '@/shared/services/default-crud.service';
 import { CustomError, getErrorWithDefault } from '@/shared/lib/custom-error';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UpdateUserArgs } from '@/modules/users/dtos/update.dto';
+import { Verification } from '@/modules/users/entities/verification.entity';
+import { QueryRunner } from 'typeorm/query-runner/QueryRunner';
 
 @Injectable()
 export class UsersService implements DefaultCRUD<User> {
 	constructor(
+		@Inject() private readonly dataSource: DataSource,
 		@InjectRepository(User) private readonly userRepository: Repository<User>,
+		@InjectRepository(Verification)
+		private readonly verificationRepository: Repository<Verification>,
 	) {}
 
 	async create({ email, role, password }: CreateUserArgs): Promise<User> {
+		/**
+		 * Transaction
+		 * @see https://docs.nestjs.com/techniques/database#typeorm-transactions
+		 */
+		const queryRunner = this.dataSource.createQueryRunner();
+		await queryRunner.connect();
+		await queryRunner.startTransaction();
+
 		try {
 			const existUser = await this.userRepository.findOne({
 				where: { email },
@@ -26,18 +39,32 @@ export class UsersService implements DefaultCRUD<User> {
 				});
 			}
 
-			const user = this.userRepository.create({
-				email,
-				role,
-				password,
-			});
+			const user = await queryRunner.manager.save(
+				this.userRepository.create({
+					email,
+					role,
+					password,
+				}),
+			);
 
-			return await this.userRepository.save(user);
+			await queryRunner.manager.save(
+				this.verificationRepository.create({
+					user,
+				}),
+			);
+
+			await queryRunner.commitTransaction();
+			return user;
 		} catch (e) {
+			// откатываем обратно наши операции с БД
+			await queryRunner?.rollbackTransaction();
 			throw getErrorWithDefault(e, {
 				errorCode: 400,
 				message: `Не удалось создать пользователя с email: ${email}`,
 			});
+		} finally {
+			// закрываем соединение наше с queryRunner
+			await queryRunner?.release();
 		}
 	}
 
