b72dddfa7692e03b24b41503d25675fa47aa7491
Роман <negativ12fly@gmail.com>
2023-12-30 03:16:21 +0500

    test: тесты на обновление пользователя

--- server/src/modules/users/tests/users.service.spec.ts
+++ server/src/modules/users/tests/users.service.spec.ts
@@ -3,7 +3,10 @@ import { UsersVerifyService } from '../users-verify.service';
 import { testingModule } from './lib/mock-module';
 import { User } from '../entities/user.entity';
 import { MockRepository } from '@/shared/lib/tests/mock-repository';
-import { createMockUserData } from '@/modules/users/tests/lib/mock-user-entity';
+import {
+	createMockUserData,
+	MockUserData,
+} from '@/modules/users/tests/lib/mock-user-entity';
 import { CustomError } from '@/shared/lib/custom-error';
 import { MockDataSource } from '@/shared/lib/tests/mock-data-source';
 import { MockComponent } from '@/shared/lib/tests/mock-component';
@@ -13,6 +16,58 @@ describe('UsersService', () => {
 	let usersVerifyService: MockComponent<UsersVerifyService>;
 	let usersRepository: MockRepository<User>;
 	let dataSource: MockDataSource;
+
+	const checkTransactionSaveUser = async (
+		user: Partial<MockUserData>,
+		{
+			commitTransactionCalledTimes,
+			rollbackTransactionCalledTimes,
+			managerSaveUserCalledTimes,
+			createVerifyCalledTimes,
+		},
+	) => {
+		const mockQueryRunner = dataSource._mockQueryRunner;
+		const { createVerifyWithTransaction } = usersVerifyService;
+
+		// проверяем открытие и закрытие Transaction
+		expect(dataSource.createQueryRunner).toHaveBeenCalledTimes(1);
+		expect(mockQueryRunner.connect).toHaveBeenCalledTimes(1);
+		expect(mockQueryRunner.startTransaction).toHaveBeenCalledTimes(1);
+		expect(mockQueryRunner.release).toHaveBeenCalledTimes(1);
+
+		expect(mockQueryRunner.commitTransaction).toHaveBeenCalledTimes(
+			commitTransactionCalledTimes,
+		);
+		expect(mockQueryRunner.rollbackTransaction).toHaveBeenCalledTimes(
+			rollbackTransactionCalledTimes,
+		);
+
+		if (managerSaveUserCalledTimes) {
+			expect(mockQueryRunner.manager.save).toHaveBeenNthCalledWith(
+				managerSaveUserCalledTimes,
+				user,
+			);
+			expect(mockQueryRunner.manager.save).nthReturnedWith(
+				managerSaveUserCalledTimes,
+				user,
+			);
+		} else {
+			expect(mockQueryRunner.manager.save).not.toHaveBeenCalled();
+		}
+
+		if (createVerifyCalledTimes) {
+			expect(createVerifyWithTransaction).toHaveBeenCalledTimes(
+				createVerifyCalledTimes,
+			);
+			expect(createVerifyWithTransaction).toHaveBeenNthCalledWith(
+				createVerifyCalledTimes,
+				user,
+				mockQueryRunner,
+			);
+		} else {
+			expect(createVerifyWithTransaction).not.toHaveBeenCalled();
+		}
+	};
 
 	beforeAll(async () => {
 		const components = await testingModule({
@@ -72,10 +127,8 @@ describe('UsersService', () => {
 	describe('create', () => {
 		it('Пользователя нету - вернули пользователя', async () => {
 			const mockUser = createMockUserData();
-			const mockQueryRunner = dataSource._mockQueryRunner;
-			const { createVerifyWithTransaction } = usersVerifyService;
 
-			usersRepository.findOne.mockResolvedValue(null);
+			usersRepository.findOne.mockResolvedValue(undefined);
 			usersRepository.create.mockResolvedValue(mockUser);
 
 			const createdUser = await usersService.create({
@@ -87,38 +140,21 @@ describe('UsersService', () => {
 			// проверим что пользователь нормальный создался
 			expect(createdUser).toBe(mockUser);
 
-			// проверяем открытие и закрытие Transaction
-			expect(dataSource.createQueryRunner).toHaveBeenCalledTimes(1);
-			expect(mockQueryRunner.connect).toHaveBeenCalledTimes(1);
-			expect(mockQueryRunner.startTransaction).toHaveBeenCalledTimes(1);
-			expect(mockQueryRunner.commitTransaction).toHaveBeenCalledTimes(1);
-			expect(mockQueryRunner.rollbackTransaction).toHaveBeenCalledTimes(0);
-			expect(mockQueryRunner.release).toHaveBeenCalledTimes(1);
-			expect(mockQueryRunner.manager.save).toHaveBeenCalledTimes(1);
-
-			// проверим сохранение в БД пользователя
-			expect(mockQueryRunner.manager.save).toHaveBeenNthCalledWith(1, mockUser);
-			expect(mockQueryRunner.manager.save).nthReturnedWith(1, mockUser);
-
-			// проверим создание Подтвержедния
-			expect(createVerifyWithTransaction).toHaveBeenCalledTimes(1);
-			expect(createVerifyWithTransaction).toHaveBeenNthCalledWith(
-				1,
-				mockUser,
-				mockQueryRunner,
-			);
+			// проверим создание пользователя
+			await checkTransactionSaveUser(mockUser, {
+				rollbackTransactionCalledTimes: 0,
+				commitTransactionCalledTimes: 1,
+				managerSaveUserCalledTimes: 1,
+				createVerifyCalledTimes: 1,
+			});
 		});
 
 		it('Пользователя нету, упало создание подтверждения - ошибка', async () => {
 			const mockUser = createMockUserData();
-			const mockQueryRunner = dataSource._mockQueryRunner;
-			const { createVerifyWithTransaction } = usersVerifyService;
 
-			usersRepository.findOne.mockResolvedValue(null);
+			usersRepository.findOne.mockResolvedValue(undefined);
 			usersRepository.create.mockResolvedValue(mockUser);
-			usersVerifyService.createVerifyWithTransaction.mockImplementation(() => {
-				throw new Error();
-			});
+			usersVerifyService.createVerifyWithTransaction.mockRejectedValue(new Error());
 
 			try {
 				await usersService.create({
@@ -131,22 +167,13 @@ describe('UsersService', () => {
 				expect(e.errorCode).toBe(400);
 			}
 
-			// проверяем открытие и закрытие Transaction
-			expect(dataSource.createQueryRunner).toHaveBeenCalledTimes(1);
-			expect(mockQueryRunner.connect).toHaveBeenCalledTimes(1);
-			expect(mockQueryRunner.startTransaction).toHaveBeenCalledTimes(1);
-			expect(mockQueryRunner.commitTransaction).toHaveBeenCalledTimes(0);
-			expect(mockQueryRunner.rollbackTransaction).toHaveBeenCalledTimes(1);
-			expect(mockQueryRunner.release).toHaveBeenCalledTimes(1);
-			expect(mockQueryRunner.manager.save).toHaveBeenCalledTimes(1);
-
-			// проверим создание Подтвержедния
-			expect(createVerifyWithTransaction).toHaveBeenCalledTimes(1);
-			expect(createVerifyWithTransaction).toHaveBeenNthCalledWith(
-				1,
-				mockUser,
-				mockQueryRunner,
-			);
+			// проверим создание пользователя
+			await checkTransactionSaveUser(mockUser, {
+				rollbackTransactionCalledTimes: 1,
+				commitTransactionCalledTimes: 0,
+				managerSaveUserCalledTimes: 1,
+				createVerifyCalledTimes: 0,
+			});
 		});
 
 		it('Пользователь уже есть - ошибка', async () => {
@@ -173,5 +200,98 @@ describe('UsersService', () => {
 		});
 	});
 
-	describe('update', () => {});
+	describe('update', () => {
+		it('При обновление почты', async () => {
+			const mockUser = createMockUserData({ isVerified: true });
+			const updateData = {
+				email: 'space@mail.ru',
+				password: '12345678',
+			};
+			usersRepository.findOne.mockResolvedValue(mockUser);
+			const updatedUser = await usersService.update(mockUser.id, updateData);
+
+			expect(updatedUser).toMatchObject({
+				...mockUser,
+				...updateData,
+				isVerified: false,
+			});
+
+			// проверим создание пользователя
+			await checkTransactionSaveUser(mockUser, {
+				rollbackTransactionCalledTimes: 0,
+				commitTransactionCalledTimes: 1,
+				managerSaveUserCalledTimes: 1,
+				createVerifyCalledTimes: 1,
+			});
+		});
+
+		it('При обновления только пароля', async () => {
+			const mockUser = createMockUserData({ isVerified: true });
+			const updateData = {
+				email: mockUser.email,
+				password: '12345678',
+			};
+			usersRepository.findOne.mockResolvedValue(mockUser);
+			const updatedUser = await usersService.update(mockUser.id, updateData);
+
+			expect(updatedUser).toMatchObject({
+				...mockUser,
+				...updateData,
+				isVerified: true,
+			});
+
+			// проверим создание пользователя
+			await checkTransactionSaveUser(mockUser, {
+				rollbackTransactionCalledTimes: 0,
+				createVerifyCalledTimes: 0,
+				commitTransactionCalledTimes: 1,
+				managerSaveUserCalledTimes: 1,
+			});
+		});
+
+		it('Упала ошибка на создание подтверждения - ошибка', async () => {
+			const mockUser = createMockUserData();
+			const updateData = {
+				email: 'space@mail.ru',
+				password: '12345678',
+			};
+
+			usersRepository.findOne.mockResolvedValue(mockUser);
+			usersVerifyService.createVerifyWithTransaction.mockRejectedValue(new Error());
+
+			try {
+				await usersService.update(mockUser.id, updateData);
+			} catch (e) {
+				expect(e).toBeInstanceOf(CustomError);
+				expect(e.errorCode).toBe(400);
+			}
+
+			// проверим создание пользователя
+			await checkTransactionSaveUser(mockUser, {
+				rollbackTransactionCalledTimes: 1,
+				managerSaveUserCalledTimes: 1,
+				commitTransactionCalledTimes: 0,
+				createVerifyCalledTimes: 0,
+			});
+		});
+
+		it('Попытались обновить пользователя, которого нет - ошибка', async () => {
+			const mockUser = createMockUserData();
+			usersRepository.findOne.mockResolvedValue(undefined);
+
+			try {
+				await usersService.update(mockUser.id, {
+					email: 'newEmail@mail.ru',
+					password: '1234568910',
+				});
+			} catch (e) {
+				expect(e).toThrow(CustomError);
+				expect(e.errorCode).toBe(400);
+			}
+
+			expect(dataSource.createQueryRunner).not.toHaveBeenCalled();
+			expect(dataSource._mockQueryRunner.manager.save).not.toHaveBeenCalled();
+			expect(usersVerifyService.createVerifyWithTransaction).not.toHaveBeenCalled();
+		});
+	});
 });
