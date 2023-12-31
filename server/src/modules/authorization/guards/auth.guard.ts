7abef92db428c360b3f4f5ea836e6742235b6ba5
Роман <negativ12fly@gmail.com>
2023-12-31 21:08:56 +0500

    feat: изменил логику AuthGuard. Теперь все resolver по дефолту идут, как приватные - доступные только авторизированному пользователю

--- server/src/modules/authorization/guards/auth.guard.ts
+++ server/src/modules/authorization/guards/auth.guard.ts
@@ -11,15 +11,34 @@ export class AuthGuard implements CanActivate {
 
 	canActivate(context: ExecutionContext): boolean {
 		const isPublic = this.reflector.get<boolean>(PUBLIC_KEY, context.getHandler());
-		const roles = this.reflector.get<USER_ROLE[]>(USER_ROLE_KEY, context.getHandler());
+		const roles = this.reflector.get<USER_ROLE[]>(
+			USER_ROLE_KEY,
+			context.getHandler(),
+		);
 
-		if (isPublic || !roles.length) {
+		if (isPublic) {
 			return true;
 		}
 
 		const gqlContext = GqlExecutionContext.create(context).getContext();
 		const user: User = gqlContext.user;
 
-		return user && roles.includes(user.role);
+		return user && this.isValidRole(user, roles);
+	}
+
+	private isValidRole(user: User, roles: USER_ROLE[]): boolean {
+		// Если не передали роли, то все подходят
+		if (!roles) {
+			return true;
+		}
+
+		// чтоб пустышки не передавали
+		if (!roles.length) {
+			throw new Error('Список Ролей не должны быть пустым');
+		}
+
+		// а тут уже проверяем на то, что роль текущего пользователя
+		// входит в список доступных ролей
+		return roles.includes(user.role);
 	}
 }
