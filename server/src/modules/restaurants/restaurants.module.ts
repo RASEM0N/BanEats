ca43741877eb24b3886a44e9c083e94cd5fcc1aa
Роман <negativ12fly@gmail.com>
2024-01-03 02:53:23 +0500

    feat: добавил для Блюд resolver и service

--- server/src/modules/restaurants/restaurants.module.ts
+++ server/src/modules/restaurants/restaurants.module.ts
@@ -6,14 +6,27 @@ import { Restaurant } from './entities/restaurant.entity';
 import { RestaurantsCategory } from './entities/category.entity';
 import { CategoryService } from './category.service';
 import { CategoryResolver } from './category.resolver';
+import { DishResolver } from './dish.resolver';
+import { DishService } from './dish.service';
+import { RestaurantDish } from './entities/dish.entity';
 
 @Module({
-	imports: [TypeOrmModule.forFeature([Restaurant, RestaurantsCategory])],
+	imports: [
+		TypeOrmModule.forFeature([
+			// ---
+			Restaurant,
+			RestaurantsCategory,
+			RestaurantDish,
+			// ---
+		]),
+	],
 	providers: [
 		RestaurantsResolver,
-		CategoryResolver,
 		RestaurantsService,
+		CategoryResolver,
 		CategoryService,
+		DishResolver,
+		DishService,
 	],
 })
 export class RestaurantsModule {}
