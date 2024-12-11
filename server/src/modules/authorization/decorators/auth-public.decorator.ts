e28912b40777b94b474a0739e814f3f1ec35dd79
RASEM0N <70789810+RASEM0N@users.noreply.github.com>
2024-12-11 19:23:17 +0500

    refactor: в Symbol.for string вынес

--- server/src/modules/authorization/decorators/auth-public.decorator.ts
+++ server/src/modules/authorization/decorators/auth-public.decorator.ts
@@ -1,4 +1,4 @@
 import { SetMetadata } from '@nestjs/common';
 
-export const META_KEY = 'is-public';
+export const META_KEY = Symbol.for('auth-public');
 export const AuthPublic = () => SetMetadata(META_KEY, true);
--- server/src/modules/authorization/decorators/auth-role.decorator.ts
+++ server/src/modules/authorization/decorators/auth-role.decorator.ts
@@ -1,5 +1,5 @@
 import { USER_ROLE } from '@/modules/users/entities/user.entity';
 import { SetMetadata } from '@nestjs/common';
 
-export const META_KEY = 'available-user-roles';
+export const META_KEY = Symbol.for('available-user-roles');
 export const AuthRoles = (roles: USER_ROLE[]) => SetMetadata(META_KEY, roles);
