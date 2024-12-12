9740269caca36a0edb006fe50e7c27109f151dd6
RASEM0N <70789810+RASEM0N@users.noreply.github.com>
2024-12-13 01:14:29 +0500

    chore: Расскоментил AuthGuard

--- server/src/modules/authorization/auth.module.ts
+++ server/src/modules/authorization/auth.module.ts
@@ -5,19 +5,20 @@ import { AuthGuard } from '@/modules/authorization/guards/auth.guard';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '@/modules/users/entities/user.entity';
 import { AuthResolver } from '@/modules/authorization/auth.resolver';
+import { UsersModule } from '@/modules/users/users.module';
 
 @Module({
-	imports: [TypeOrmModule.forFeature([User])],
+	imports: [UsersModule, TypeOrmModule.forFeature([User])],
 	providers: [
 		AuthService,
 		AuthResolver,
 
 		// Глобальный декоратор
 		// https://docs.nestjs.com/security/authentication#enable-authentication-globally
-		// {
-		// 	provide: APP_GUARD,
-		// 	useClass: AuthGuard,
-		// },
+		{
+			provide: APP_GUARD,
+			useClass: AuthGuard,
+		},
 	],
 })
 export class AuthModule {}
