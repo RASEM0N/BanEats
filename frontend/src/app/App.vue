02714c46856c2b00256af34514356742f70270e2
RASEM0N <70789810+RASEM0N@users.noreply.github.com>
2024-12-17 01:17:22 +0500

    chore: добавил болванку useQuery

--- frontend/src/app/App.vue
+++ frontend/src/app/App.vue
@@ -1,6 +1,35 @@
-<script lang="ts" setup>
+<script setup lang="ts">
+import { useQuery } from '@vue/apollo-composable';
+import gql from 'graphql-tag';
+import { watch } from 'vue';
+
+const { result, loading, error, refetch, onResult } = useQuery<string>(gql`
+	query {
+		testHello
+	}
+`, null, { fetchPolicy: 'no-cache' });
+
+const value = () => {
+	refetch();
+};
+
+onResult((param, context) => {
+	console.log(`RESULT HAS ESDGLDKS`);
+	console.log(context);
+	console.log(param);
+	console.log(param.data);
+	console.log(result.value);
+});
+
+watch(result, () => {
+	console.log(`Was loaded: `, result.value);
+});
+
 </script>
 <template>
-	<h1>Hello</h1>
+	<h1>RESULT: {{ result }}</h1>
+	<h1>LOADING: {{ loading }}</h1>
+	<h1>ERROR: {{ error }}</h1>
+	<button @click="value">FETCH</button>
 </template>
 <style></style>
--- frontend/src/app/index.ts
+++ frontend/src/app/index.ts
@@ -2,20 +2,25 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import './index.css';
 import App from './App.vue';
-import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
 import { DefaultApolloClient } from '@vue/apollo-composable';
+import { InMemoryCache, ApolloClient, createHttpLink, HttpLink } from '@apollo/client/core';
 
 // https://apollo.vuejs.org/guide/installation.html
-// const apolloClient = new ApolloClient({
-// 	cache: new InMemoryCache(),
-//
-// 	// @TODO вынести
-// 	url: createHttpLink({ uri: 'http://localhost:8000/graphql' }),
-// });
+const apolloClient = new ApolloClient({
+	cache: new InMemoryCache(),
+	devtools: {
+		enabled: true,
+		name: 'UberEatsClient',
+	},
+	// @TODO из ENV получать
+	uri: 'http://localhost:8000/graphql',
+});
+
+// @TODO почему с этеми не работает нормально
+// console.log(createHttpLink({ uri: 'http://localhost:8000/graphql' }));
+// console.log(new HttpLink({ uri: 'http://localhost:8000/graphql' }));
 
 createApp(App)
 	.use(createPinia())
-
-	// https://apollo.vuejs.org/guide-composable/setup.html
-	// .use(DefaultApolloClient, apolloClient)
+	.provide(DefaultApolloClient, apolloClient)
 	.mount('#app');
