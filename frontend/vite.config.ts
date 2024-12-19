83adc2b7226898cba55a8be2834bc3d32a261f90
RASEM0N <70789810+RASEM0N@users.noreply.github.com>
2024-12-20 03:19:51 +0500

    feat: добавил svgr + иконка лога eats

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
+import svgLoader from 'vite-svg-loader';
 import vue from '@vitejs/plugin-vue';
 import path from 'node:path';
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [vue()],
+	plugins: [vue(), svgLoader()],
 	resolve: {
 		alias: {
 			'@app': path.join(__dirname, 'src/app'),
