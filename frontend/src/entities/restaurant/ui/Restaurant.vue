8d2de9c74476e46040427d5bff09ac844759c12e
RASEM0N <70789810+RASEM0N@users.noreply.github.com>
2024-12-24 23:50:10 +0500

    chore(front): добавил ссылОчку

--- frontend/src/entities/restaurant/ui/Restaurant.vue
+++ frontend/src/entities/restaurant/ui/Restaurant.vue
@@ -9,16 +9,17 @@ interface Props {
 const { restaurant } = defineProps<Props>();
 </script>
 <template>
-	<div class="flex flex-col">
-		<div class="bg-cover bg-center mb-3 py-28"
-			 :style="{ backgroundImage: `url(${restaurant.coverImage})` }"
-		></div>
-		<h3 class="text-xl">
-			{{ restaurant.id }}: {{ restaurant.name }}
-		</h3>
-		<span v-if="restaurant.category"
-			  class="border-t mt-2 py-2 text-xs opacity-50 border-gray-400">
-			{{ restaurant.category?.name }}
-		</span>
-	</div>
+	<router-link :to="`/restaurants/${restaurant.id}`">
+		<div class="flex flex-col">
+			<div class="bg-cover bg-center mb-3 py-28"
+				 :style="{ backgroundImage: `url(${restaurant.coverImage})` }"
+			></div>
+			<h3 class="text-xl">
+				{{ restaurant.id }}: {{ restaurant.name }}
+			</h3>
+			<span class="border-t mt-2 py-2 text-xs opacity-50 border-gray-400">
+				{{ restaurant.category?.name }}
+			</span>
+		</div>
+	</router-link>
 </template>
