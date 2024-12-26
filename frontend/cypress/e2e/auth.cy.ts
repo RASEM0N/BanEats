d62da6a47d66645bab10e5a8ce1476340e551009
RASEM0N <70789810+RASEM0N@users.noreply.github.com>
2024-12-26 10:10:30 +0500

    test(front): тест на редактирования данных пользователя

--- frontend/cypress/e2e/auth.cy.ts
+++ frontend/cypress/e2e/auth.cy.ts
@@ -23,4 +23,22 @@ describe('Authorization', () => {
 
 		cy.url().should('include', '/restaurants');
 	});
+
+	it('Edit user', () => {
+		const newEmail = `email${new Date().getTime()}@mail.ru`;
+
+		cy.visit('/');
+		cy.get('form input[type="email"]').type(email);
+		cy.get('form input[type="password"]').type(password).type('{enter}');
+
+		cy.get('#nav-edit-profile').click();
+		cy.url().should('include', '/edit-profile');
+
+		cy.get('form input[type="email"]').clear()
+		cy.get('form input[type="email"]').type(newEmail).type('{enter}');
+
+		cy.get('[data-testid]').should('exist');
+		cy.get('form input[type="email"]').should('have.value', newEmail);
+		cy.get('form input[type="password"]').should('have.value', '');
+	});
 });
