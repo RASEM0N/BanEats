f9b8001408f668db6d33461328c706dd5ba10931
RASEM0N <70789810+RASEM0N@users.noreply.github.com>
2024-12-27 09:46:30 +0500

    refactor(front): упростил логику, оказывается route был реактивным

--- frontend/src/shared/lib/router/refPage.ts
+++ frontend/src/shared/lib/router/refPage.ts
@@ -1,24 +1,17 @@
 import { useRoute, useRouter } from 'vue-router';
 import { ref, watch } from 'vue';
-import { onHistory } from './history';
 
 export const refPage = (value: number = 1) => {
 	const route = useRoute();
 	const router = useRouter();
 	const page = ref(normalizeQuery(route.query['page'], value));
 
-	// @TODO
-	// тут вроде как можно сделать
-	// watch(() => route.query.page, (page) => ...
-
-	// page.value обновляем значение, чтоб при Prev/Next перендарилось
-	onHistory(() => {
-		page.value = normalizeQuery(queryFromLocation(), value);
+	watch(() => route.query['page'] as string, (newPage: string) => {
+		page.value = normalizeQuery(newPage, value);
 	});
 
-	// Синхранизируем page.value с ?page= в URL
 	watch(page, (page) => {
-		const query = normalizeQuery(queryFromLocation(), value);
+		const query = normalizeQuery(route.query['page'], value);
 
 		if (query !== page) {
 			router.push({ query: { page } });
@@ -31,8 +24,4 @@ export const refPage = (value: number = 1) => {
 const normalizeQuery = (query?: any, value: number = 1) => {
 	const numQuery = Number(query);
 	return isNaN(numQuery) ? value : numQuery;
-};
-
-const queryFromLocation = () => {
-	return new URL(window.location.toString()).searchParams.get('page');
 };
